a91f7e6a869c1ace41ce0426424b6e7b9c3225ec
Danielle-79 <danrui@ualberta.ca>
2026-03-04 17:44:57 -0700

    lab08fail-2

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -1,15 +1,17 @@
 package com.example.lab08;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.example.lab08.CustomList;
 
 public class CustomListTest {
-
     @Test
     public void testHasCity() {
         CustomList list = new CustomList();
         City calgary = new City("Calgary", "AB");
         list.addCity(calgary);
+// This will fail initially because hasCity() doesn‘t exist
         assertTrue(list.hasCity(calgary));
     }
+
 }
